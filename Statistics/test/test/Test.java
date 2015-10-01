e860d97b519a5a0cc96464eda36a85660cef32dd
ailopera <lopera.martinez.ana.isabel@gmail.com>
2015-10-01 13:21:33 +0200

    Java no es Python

--- Statistics/test/test/Test.java
+++ Statistics/test/test/Test.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Array;
 import java.util.Vector;
 
 import statistics.Statistics;
@@ -10,32 +11,41 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class StatisticsTest {
-	// Creamos un objeto Statistics formado por un vector vacio
-	Statistics null_data = new Statistics(null);
-	
-	//Creamos un objeto Statistics formado por un vector con un elemento
-	Vector one_value = new Vector();
-	one_value.addElement((float)0);
-	Statistics one_data = new Statistics(one_value);
-		
-	
-	//Creamos un objeto Statistics formado por un vector con diez elementos
-	Vector ten_values = new Vector();
-	for (int j= 0; j < 2; j++ ) {
-		for (int i = 0; i < 10; i++) {
-			ten_values.addElement((float) i);
+	//Declaramos metodo auxiliar para rellenar los vectores de prueba
+	public void filldata(Vector<Float> vector, int size){
+		for (int j= 0; j < 2; j++ ) {
+			for (int i = 0; i < size; i++) {
+				vector.addElement((float) i);
+			}
 		}
 	}
+	// Creamos un objeto Statistics formado por un vector vacio
+	Statistics null_data = new Statistics(null);
+
+	//Creamos un objeto Statistics formado por un vector con un elemento
+	Vector<Float> one_value = new Vector<Float>();
+	one_value.addElement(0);
+	Statistics one_data = new Statistics(one_value);
+
+
+	//Creamos un objeto Statistics formado por un vector con diez elementos
+	Vector<Float> ten_values = new Vector<Float>();
+	filldata(ten_values,10);
 	Statistics ten_data = new Statistics(ten_values);
-	
-	
+
+
 	// Pruebas relativas a la media
 	@Test
 	public void average_null_data(){
 		assertEquals(0,null_data.average());
 	}
-	
+
+	@Test
+	public void average_one_data(){
+		assertEquals(0,one_data.average());
+	}
+
 	// Pruebas relativas a la dist. tipica
-	
+
 	// Pruebas relativas al histograma
-	
+
